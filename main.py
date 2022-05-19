0f03dfd1180c08c217399039c7eab3322bccbd2a
Green Hexagon <91151253+GreenHexagon@users.noreply.github.com>
2022-05-19 16:30:50 +0000

    +rgbcolortype

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ def type(w, delay = 0.25, instant=False, newline=True):
     sleep(delay)
   if newline == True:
     print("")
+def rgbcolortype(w, r, g, b, delay=0.25, instant=False, newline=True):
+  if instant == True and delay != 0:
+    delay =0
+  sys.stdout.write(f"\033[38;2;{r};{g};{b}m")
+  sys.stdout.flush()
+  for c in w:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    sleep(delay)
+  if newline == True:
+    print("")
+  print("\033[0m")
