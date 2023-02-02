d8eb17120dd4b716a10b40012477a2d25a80364b
Green Hexagon <91151253+GreenHexagon@users.noreply.github.com>
2023-02-02 14:39:36 -0500

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+import sys, os
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.ifconfig']
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = u'Type'
+copyright = u'2023, GreenHexagon'
+
+version = '0.1'
+
+release = '0.1.0b'
