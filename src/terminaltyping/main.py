e54c9cc51a26a83bcf9041fb8b923d86a6bf3986
Green Hexagon <91151253+GreenHexagon@users.noreply.github.com>
2023-02-07 09:26:43 -0500

    Typing colors (#2)
    * Added TypingColors Class
    
    * TypingColors Update
    
    * Added A bunch of stuff

--- src/terminaltyping/main.py
+++ src/terminaltyping/main.py
@@ -1,6 +1,34 @@
 """Terminal Typing, c 2023, GreenHexagon"""
 import sys
 from time import sleep
+from argparse import ArgumentParser
+from importlib.metadata import version
+
+
+def entry_point():
+    """Version"""
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--version", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.version:
+        print(version("gha_python_packaging_demo"))
+        sys.exit()
+
+class TypingColor():
+    """Class for Colors"""
+    def __init__(self,r:int,g:int,b:int):
+        if r > 255:
+            raise ValueError
+        if g > 255:
+            raise ValueError
+        if b > 255:
+            raise ValueError
+        self.r = r
+        self.g = g
+        self.b = b
+        self.rgb = str(r) + str(g) + str(b)
 
 class TypingColor():
   """Class for Colors"""
@@ -17,25 +45,25 @@ class TypingColor():
       self.rgb = str(r) + str(g) + str(b)
 
 class Typing():
-  """Top level class for module"""
-  def t_type(self, word:str, rgb:list | TypingColor | None, delay=0.25, newline=True):
-    """Base typing module"""
-    if rgb is None:
-      rgb = TypingColor(0,0,0)
-    if isinstance(rgb, list):
-      r = rgb[0]
-      g = rgb[1]
-      b = rgb[2]
-    if isinstance(rgb, TypingColor):
-      r = rgb.r
-      g = rgb.g
-      b = rgb.b
-    sys.stdout.write(f"\033[38;2;{r};{g};{b}m")
-    sys.stdout.flush()
-    for char in word:
-      sys.stdout.write(char)
-      sys.stdout.flush()
-      sleep(delay)
-    if newline:
-      print("")
-      print("\033[0m")
+    """Top level class for module"""
+    def t_type(self, word:str, rgb:list | TypingColor | None, delay=0.25, newline=True):
+        """Base typing module"""
+        if rgb is None:
+            rgb = TypingColor(0,0,0)
+        if isinstance(rgb, list):
+            r = rgb[0]
+            g = rgb[1]
+            b = rgb[2]
+        if isinstance(rgb, TypingColor):
+            r = rgb.r
+            g = rgb.g
+            b = rgb.b
+        sys.stdout.write(f"\033[38;2;{r};{g};{b}m")
+        sys.stdout.flush()
+        for char in word:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            sleep(delay)
+        if newline:
+            print("")
+        print("\033[0m")
